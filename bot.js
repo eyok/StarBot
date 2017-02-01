4a8cee425d59c6b717d6b88c545cc61875d17354
eyok <yd69655@uga.edu>
2017-02-01 14:51:35 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,51 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/meme$/,
-      botRegey = /^\/link$/,
-      botRegez = /^\/sign$/,
-      botRegea = /^\/bot$/,
-      botRegeb = /^\/GoogleVoiceSignIn$/;
+      botRegex = /^\/meme$/;
   
-  if(request.text && botRegeb.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage5();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
-  if(request.text && botRegey.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage2();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
-  if(request.text && botRegez.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage3();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
-  if(request.text && botRegea.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage4();
     this.res.end();
   } else {
     console.log("don't care");
@@ -92,126 +52,6 @@ function postMessage() {
   });
   botReq.end(JSON.stringify(body));
 }
-
-function postMessage2() {
-  var botResponse, options, body, botReq;
-
-  botResponse = "http://www.ordercosmic.com/admin?verifyMe=rff8e7f5f5w";
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-
-function postMessage3() {
-  var botResponse, options, body, botReq;
-
-  botResponse = "~ If you don't have a Cosmic Sign then we won't help you pay for the parking ticket. ~";
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-
-function postMessage4() {
-  var botResponse, options, body, botReq;
-
-  botResponse = "*Bzzz* I am Cosmobot. I can do things like /bot, /meme, /link, /GoogleVoiceSignIn and /sign. *Bzzz*";
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-function postMessage5() {
-  var botResponse, options, body, botReq;
-
-  botResponse = "Do it yourself lazy. I'm not that smart.";
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
