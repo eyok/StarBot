33148ac138228221ec6656b543ee72583a5dc687
eyok <yd69655@uga.edu>
2017-03-25 09:42:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,23 +5,84 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/meme$/;
-
-  if(request.text && botRegex.test(request.text)) {
+      meme = /^\/meme$/,
+      link = /^\/link$/,
+      sign = /^\/sign$/,
+      help = /^\/help$/,
+      reserve = /^\/reserve$/,
+      drop = /^\/drop$/,
+      shifts = /^\/shifts$/;
+  
+  if(request.text && meme.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(cool());
     this.res.end();
   } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
-}
+  if(request.text && link.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.ordercosmic.com/admin?verifyMe=39Efkeo93j");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+    if(request.text && sign.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("~Please have a Cosmic Topper or sign, if you don't we won't help you pay for your ticket.~");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+    if(request.text && help.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Cosmobot Commands: /link, /sign, /help (but you knew that), /reserve, /drop, and /shifts. Enjoy!");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+  if(request.text && reserve.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://docs.google.com/a/switchit001.com/forms/d/1nCN3yKNGigaxiI2NOh7sDwrnwf4AvYDwlZujGDFHg_w/edit");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+  if(request.text && drop.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://docs.google.com/a/switchit001.com/forms/d/1nCN3yKNGigaxiI2NOh7sDwrnwf4AvYDwlZujGDFHg_w/edit");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+  if(request.text && shifts.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://docs.google.com/document/d/1s4FsTDc3tbZMoiKq51gl26msLW2Op2VUhlQTHTtG1bg/edit");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
 
-function postMessage() {
+}//end respond()
+
+function postMessage(s) {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = s;
 
   options = {
     hostname: 'api.groupme.com',
@@ -51,7 +112,7 @@ function postMessage() {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
-}
+} //meme
 
 
 exports.respond = respond;
