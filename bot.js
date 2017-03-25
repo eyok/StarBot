35381370ad819d9f1b5a54b1e4de2346b8f3dc02
eyok <yd69655@uga.edu>
2017-03-25 09:47:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,11 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       meme = /^\/meme$/,
-      link = /^\/link$/,
-      sign = /^\/sign$/,
+      orders = /^\/orders$/,
+      records = /^\/records$/,
       help = /^\/help$/,
-      reserve = /^\/reserve$/,
-      drop = /^\/drop$/,
-      shifts = /^\/shifts$/;
+      menu = /^\/menus$/,
+      guide = /^\/guide/;
   
   if(request.text && meme.test(request.text)) {
     this.res.writeHead(200);
@@ -22,18 +21,18 @@ function respond() {
     this.res.writeHead(200);
     this.res.end();
   }
-  if(request.text && link.test(request.text)) {
+  if(request.text && orders.request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.ordercosmic.com/admin?verifyMe=39Efkeo93j");
+    postMessage("www.ordercosmic.com/admin/orders?verifyMe=rff8e7f5f5w");
     this.res.end();
   } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
-    if(request.text && sign.test(request.text)) {
+    if(request.text && records.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("~Please have a Cosmic Topper or sign, if you don't we won't help you pay for your ticket.~");
+    postMessage("https://docs.google.com/spreadsheets/d/1vGXOkNTHAcKaxb5w_8uoJC1gsGAoKaBQN5jsHdtOzkk/edit#gid=0");
     this.res.end();
   } else {
     console.log("don't care");
@@ -42,32 +41,23 @@ function respond() {
   }
     if(request.text && help.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("Cosmobot Commands: /link, /sign, /help (but you knew that), /reserve, /drop, and /shifts. Enjoy!");
+    postMessage("Cosmobot Commands: /orders, /records, /help (but you knew that), /menu, /guide. Enjoy!");
     this.res.end();
   } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
-  if(request.text && reserve.test(request.text)) {
+  if(request.text && menu.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://docs.google.com/a/switchit001.com/forms/d/1nCN3yKNGigaxiI2NOh7sDwrnwf4AvYDwlZujGDFHg_w/edit");
+    postMessage("www.ordercosmic.com/admin/franchises?verifyMe=rff8e7f5f5w");
     this.res.end();
   } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
-  if(request.text && drop.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("https://docs.google.com/a/switchit001.com/forms/d/1nCN3yKNGigaxiI2NOh7sDwrnwf4AvYDwlZujGDFHg_w/edit");
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
-  if(request.text && shifts.test(request.text)) {
+  if(request.text && guide.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://docs.google.com/document/d/1s4FsTDc3tbZMoiKq51gl26msLW2Op2VUhlQTHTtG1bg/edit");
     this.res.end();
